dc57d084da09b9b0ca39434dec77184e95480153
weilderbeast <bogdan.is.a.beast@gmail.com>
2022-12-01 13:08:55 +0100

    comment

--- src/models/data-source.ts
+++ src/models/data-source.ts
@@ -4,6 +4,8 @@ import { Directors, Movies, People, Ratings, Stars } from "./models";
 export const dataSource = new DataSource({
   type: "postgres",
   host: process.env.REACT_APP_HOST,
+  // env vars in react need the REACT_APP prefix to work
+  // and they always return as (string | undefined)
   port: parseInt(process.env.REACT_APP_PORT!),
   username: process.env.REACT_APP_USERNAME,
   password: process.env.REACT_APP_PASSWORD,
