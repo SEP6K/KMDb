066eb8070c242f2e9ad794f0aca23c4b88134d88
IonutGrosu <ionutflorentin.grosu@gmail.com>
2022-12-02 11:40:10 +0100

    fixed environment variables naming and firebase sign in methods

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,15 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import React from "react";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { useState } from "react";
 import { auth } from "../../utils/firebase";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
     try {
@@ -13,8 +20,39 @@ const Register = () => {
     }
   };
 
+  const signUpWithEmailAndPass = async () => {
+    createUserWithEmailAndPassword(auth, "ionut@gmail.com", "password")
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error);
+        // ..
+      });
+  };
+
   return (
     <div>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit" onClick={() => signUpWithEmailAndPass()}>
+        Register
+      </button>
       <button onClick={GoogleLogin}>Sign in with Google</button>
     </div>
   );
