2827e785da49c84212a96256f0c9914e7d9e6383
weilderbeast <bogdan.is.a.beast@gmail.com>
2022-12-06 11:03:16 +0100

    update env vars

--- src/models/data-source.ts
+++ src/models/data-source.ts
@@ -3,12 +3,12 @@ import { Directors, Movies, People, Ratings, Stars } from "./models";
 
 export const dataSource = new DataSource({
   type: "postgres",
-  host: process.env.REACT_APP_HOST,
+  host: import.meta.env.VITE_HOST,
   // env vars in react need the REACT_APP prefix to work
   // and they always return as (string | undefined)
-  port: parseInt(process.env.REACT_APP_PORT!),
-  username: process.env.REACT_APP_USERNAME,
-  password: process.env.REACT_APP_PASSWORD,
-  database: process.env.REACT_APP_DATABASE,
+  port: parseInt(import.meta.env.VITE_PORT!),
+  username: import.meta.env.VITE_USERNAME,
+  password: import.meta.env.VITE_PASSWORD,
+  database: import.meta.env.VITE_DATABASE,
   entities: [Directors, Movies, People, Ratings, Stars],
 }).initialize();
