6e295482fab40469a53b11b4b72dc22a62c80a3f
IonutGrosu <ionutflorentin.grosu@gmail.com>
2022-12-06 10:57:26 +0100

    added db related env vars and updated the  dev workflow action

--- src/models/data-source.ts
+++ src/models/data-source.ts
@@ -3,12 +3,12 @@ import { Directors, Movies, People, Ratings, Stars } from "./models";
 
 export const dataSource = new DataSource({
   type: "postgres",
-  host: process.env.REACT_APP_HOST,
-  // env vars in react need the REACT_APP prefix to work
+  host: process.env.VITE_DB_HOST,
+  // env vars in react need the VITE prefix to work
   // and they always return as (string | undefined)
-  port: parseInt(process.env.REACT_APP_PORT!),
-  username: process.env.REACT_APP_USERNAME,
-  password: process.env.REACT_APP_PASSWORD,
-  database: process.env.REACT_APP_DATABASE,
+  port: parseInt(import.meta.env.VITE_DB_PORT!),
+  username: import.meta.env.VITE_DB_USERNAME,
+  password: import.meta.env.VITE_DB_PASSWORD,
+  database: import.meta.env.VITE_DB_DATABASE,
   entities: [Directors, Movies, People, Ratings, Stars],
 }).initialize();
