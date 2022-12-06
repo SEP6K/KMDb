6b29ec23a5ec30a20a52e3c61d755e8281c924ef
weilderbeast <bogdan.is.a.beast@gmail.com>
2022-12-06 11:24:58 +0100

    some styling

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -4,11 +4,13 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { auth } from "../../../utils/firebase";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
@@ -21,23 +23,31 @@ const Register = () => {
   };
 
   const signUpWithEmailAndPass = async () => {
-    createUserWithEmailAndPassword(auth, "ionut@gmail.com", "password")
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
-        console.log(user);
+        navigate("/");
         // ...
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
         console.log(error);
         // ..
       });
   };
 
   return (
-    <div>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: "16px",
+      }}
+    >
       <input
         type="email"
         placeholder="Email"
@@ -50,10 +60,38 @@ const Register = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button type="submit" onClick={() => signUpWithEmailAndPass()}>
-        Register
-      </button>
-      <button onClick={GoogleLogin}>Sign in with Google</button>
+      <div
+        style={{
+          display: "flex",
+          gap: "16px",
+          flexDirection: "column",
+          position: "relative",
+        }}
+      >
+        <button type="submit" onClick={() => signUpWithEmailAndPass()}>
+          Register
+        </button>
+        <div
+          style={{
+            height: "1px",
+            width: "100%",
+            backgroundColor: "gray",
+          }}
+        />
+        <p
+          style={{
+            position: "absolute",
+            top: "30px",
+            left: "50%",
+            translate: "-50% 0",
+            padding: "0 8px",
+            background: "black",
+          }}
+        >
+          or
+        </p>
+        <button onClick={GoogleLogin}>Google OAuth</button>
+      </div>
     </div>
   );
 };
