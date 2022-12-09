72a51873f4299b4c0b2ccbc4176e040051895d5e
weilderbeast <bogdan.is.a.beast@gmail.com>
2022-12-09 13:28:09 +0100

    add movie page with filters

---
+++ src/components/checkbox.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { BsCheck2 } from "react-icons/bs";
+
+type Props = {
+  isChecked: boolean;
+};
+
+export const Checkbox = ({ isChecked }: Props) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "24px",
+        width: "24px",
+        background: isChecked ? "#646cff" : "black",
+        borderRadius: "4px",
+        transition: ".2 background",
+      }}
+    >
+      <BsCheck2 style={{ color: "black" }} />
+    </div>
+  );
+};
